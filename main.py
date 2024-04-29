d4eb4c0b9edec52d1fdf15b35c48984c1b535932
21ftt1750 <99001373+21ftt1750@users.noreply.github.com>
2024-04-29 15:11:41 +0800

    wizrd theme

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import csv
 import os
 from dotenv import load_dotenv
 import random
-from borneoBulettin import scrape_and_save_news  # Import the function
+from borneoBulettin import scrape_and_save_news  
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -29,25 +29,24 @@ async def send_category_selection(ctx, selected_category=None):
     categories = ['national', 'sea', 'world', 'business', 'technology', 'lifestyle', 'entertainment', 'sports', 'features']
     view = discord.ui.View()
     for category in categories:
-        emoji = '📰'  # Example emojis
+        emoji = '📰'  
         button = discord.ui.Button(label=f" {category.capitalize()} {emoji}", style=discord.ButtonStyle.secondary)
         button.callback = create_button_callback(category)
         view.add_item(button)
     
     # Send the category selection message with buttons
-    message = await ctx.send("Please choose a category! 🧙🏻‍♂️🪄✨", view=view)
+    message = await ctx.send("Enter the realm of magic! 🧙🏻‍♂️ Choose a mystical category! 🪄✨", view=view)
 
 @client.event
 async def on_button_click(interaction, category):
     news = read_csv(category)
     if news:
-        await interaction.channel.send(f'Here are some of the latest **{category.capitalize()} news** headlines:')
+        await interaction.channel.send(f'Behold! Herein lie the freshest chronicles of **{category.capitalize()} news**:')
         for article in news:
             await interaction.channel.send(content=article)
     else:
         await interaction.channel.send(content=f'No {category.capitalize()} news found.')
-    
-    # After displaying news, send category selection again
+
     await send_category_selection(interaction.channel, category)
 
 @client.event
@@ -59,23 +58,25 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('!news'):  # Check if the message starts with the prefix '!news'
-        await message.channel.send("Fetching latest news, please wait...")
+    if message.content.startswith('!scrolls'):  # Check if the message starts with the prefix '!scrolls'
+        await message.channel.send("Gather ye, for the ethereal threads of the latest tidings beckon, a moment's patience I seek... 🔮")
         scrape_and_save_news()  # Call the function to scrape news
         await send_category_selection(message.channel)  # Proceed with sending the news
-    elif message.content.startswith('!help'):  # Check if the message starts with the prefix '!help'
-        await message.channel.send("Available Commands:\n- **!news**: Get the latest news from Borneo Bulletin.\n- **?news**: Get the latest news from Borneo Bulletin via DM.")
+    elif message.content.startswith('!sorcery'):  # Check if the message starts with the prefix '!sorcery'
+        await message.channel.send("Behold, the ethereal commands beckon: \n- **!scrolls**: Unravel the latest mystical scrolls from Borneo Bulletin. \n- **?scrolls**: Receive the enigmatic whispers of Borneo Bulletin's news through the mystic channels of DMs.")
     elif message.content.startswith('!'):
         # Handle other commands with the prefix
         responses = [
-            "I didn't quite catch that command 😓 Please type **'!news'** to get the latest news. **'!help'** to see all the available commands!",
-            "Can you repeat that 🤔 or type **'!news'** to start. **'!help'** to see all the commands we have!",
-            "Sorry, I don't understand that 🤨 Please use **'!news'** to begin. **'!help'** for more options!"
+            "Lost amidst the arcane whispers 🌪️ Venture forth with '**!scrolls**' for the latest lore. Unveil all secrets using '**!sorcery**' 🌟!",
+            "An enigma hangs in the air 🌀 Embrace '**!scrolls**' to uncover hidden 🧙‍♂️ realms, or invoke '**!sorcery**' for celestial guidance 🌌!",
+            "Apologies for the mystical haze 🌌 Embark on your journey with '**!scrolls**' 📜. Plunge deeper into the unknown with '**!sorcery**' ✨!"
+
+
         ]
         response = random.choice(responses)
         await message.channel.send(response)
-    elif message.content.startswith('?news'):  # Check if the message starts with the prefix '?news' for DM
-        await message.author.send("Fetching latest news, please wait...")
+    elif message.content.startswith('?scrolls'):  # Check if the message starts with the prefix '?scrolls' for DM
+        await message.author.send("Gather ye, for the ethereal threads of the latest tidings beckon, a moment's patience I seek... 🔮")
         scrape_and_save_news()  # Call the function to scrape news
         await send_category_selection(message.author)  # Send the category selection message as DM
 
